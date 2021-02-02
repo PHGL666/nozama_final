44ed700dfa55ab75702699b9b954db0d61b7fe80
PHGL666 <pierregotzsche@gmail.com>
2021-02-02 16:09:36 +0100

    create Utilities Constants -> URL

---
+++ nozama.bonus/src/test/java/Utilities/Constants.java
@@ -0,0 +1,16 @@
+  
+package Utilities;
+// Public pour pouvoir instancier/récupérer les données de la classe n'importe où
+public class Constants {
+	// Static permet de récupérer la variable ou la méthode sans instancier la classe
+	// Final permet de rendre une variable non-modifiable == constante
+	// On stocke l'URL dans une variable pour que le changement s'effectue sur tout le code
+	public static final String URL = "http://127.0.0.1/nozama";
+	// Stockage du chemin vers les différents fichiers utilitaires
+	
+	/*
+	public static final String Path_TestData = "C:\\Users\\cleme\\git\\Nozama-1\\Nozama_Admin\\src\\test\\java\\data\\";
+	// Nom du fichier qu'on pourra ajouter à la fin du path test data
+	public static final String File_TestData = "data.xls";
+	*/
+}
